bbe66da6ac3196053cf5b60221e238d74b25127d
zakhttp <zakhttp@gmail.com>
2016-08-13 18:43:19 +0400

    Init header and side nav

---
+++ js/app.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+});
+
+
+var profileData = {
+  name: 'zakaria',
+  surname: 'benbakkar',
+  email: 'zakhttp@gmail.com',
+  github: 'zakhttp',
+  freeCodeCamp: 'zakhttp',
+  twitter: 'zaksudo',
+  skills: [
+    'html5',
+    'css3',
+    'javascript',
+    'git',
+    'php',
+    'laravel',
+  ],
+  bio: '',
+  tagline: '',
+  quotes: [
+    '',
+    ''
+  ],
+  projects: [
+    {
+      name: 'idesign interiors',
+      url: 'http://idesignuae.com',
+      type: 'wordspress',
+      imagrUrl: '',
+      description: ''
+    },
+    {
+      name: 'super mario frogger',
+      url: 'https://github.com/zakhttp/super-mario-frogger',
+      type: 'javascript',
+      imagrUrl: '',
+      description: ''
+    },
+    {
+      name: 'shippey photography',
+      url: 'http://shippeyphotography.com',
+      type: 'wordpress',
+      imagrUrl: '',
+      description: ''
+    },
+    {
+      name: 'weather app',
+      url: 'https://github.com/zakhttp/weather-app',
+      type: 'react native',
+      imagrUrl: '',
+      description: ''
+    },
+  ]
+};
